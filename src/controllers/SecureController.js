0468d83ddbc4768f739674246d324337e3c2d9f6
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-10-31 23:00:10 -0500

    add secure

--- src/controllers/SecureController.js
+++ src/controllers/SecureController.js
@@ -14,14 +14,14 @@ module.exports = {
             return res.json({ message: 'error', res: 'Failed to authenticate' })
         }
         const io = req.app.io;
-        const { valueStatus } = req.body;
-        if (!valueStatus) {
-            return res.status(200).json({ message: 'error', res: 'Missing the valueStatus' })
+        const { active } = req.body;
+        if (!active) {
+            return res.status(200).json({ message: 'error', res: 'Missing the active' })
         }
         knex('secure').where('id', 1).update({
-            active: String(valueStatus)
+            active: String(active)
         }).then(() => {
-            io.emit('app_status_isBlocked', `${valueStatus}`);
+            io.emit('app_status_isBlocked', `${active}`);
             return res.status(200).json({ message: 'success' })
         }).catch(() => {
             return res.status(200).json({ message: 'error' });
